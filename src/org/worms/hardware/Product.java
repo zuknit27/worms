0059fb0377c55b42ae3ed0e19c8901052e29fe6c
damionclay <33673642+damionclay@users.noreply.github.com>
2017-11-14 18:52:38 -0700

    Update Product.java

--- src/org/worms/hardware/Product.java
+++ src/org/worms/hardware/Product.java
@@ -1,5 +1,13 @@
 package org.worms.hardware;
 
 public class Product {
-
+  private String name;
+  private double price;
+  private int inventory;
+  
+  public Product(){
+    name = "unknown";
+    price = 0;
+    inventory = 0;
+  }
 }
